39003d7c084d81de72cf23e2b6d1473828fdfe24
generallegion <generallegion@yandex.ru>
2024-11-26 01:56:34 +0400

    тестовый вывод данных

--- html/index.php
+++ html/index.php
@@ -18,12 +18,40 @@ $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 if (!$db) {
     die('Ошибка соединения: ' . mysqli_error($db));
 }
-echo 'Успешно соединились' . "<br>";
+echo 'Соединение успешно' . "<br>";
+
 
 $result = $db->query("SELECT * FROM Products");
-for ($row_no = $result->num_rows - 1; $row_no >= 0; $row_no--) {
-    $result->data_seek($row_no);
-    $row = $result->fetch_assoc();
-    echo " id = " . $row['ID'] . " name =" .$row['PRODUCT_NAME'] . "<br>";
-}
 mysqli_close($db);
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>PRODUCT_ID</th>
+            <th>PRODUCT_NAME</th>
+            <th>PRODUCT_PRICE</th>
+            <th>PRODUCT_ARTICLE</th>
+            <th>PRODUCT_QUANTITY</th>
+            <th>DATE_CREATE</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        for ($row_no = $result->num_rows - 1; $row_no >= 0; $row_no--) {
+            $result->data_seek($row_no);
+            $row = $result->fetch_assoc();
+            echo "<tr>";
+            echo "<td>" . $row['ID'] . "</td>";
+            echo "<td>" . $row['PRODUCT_ID'] . "</td>";
+            echo "<td>" . $row['PRODUCT_NAME'] . "</td>";
+            echo "<td>" . $row['PRODUCT_PRICE'] . "</td>";
+            echo "<td>" . $row['PRODUCT_ARTICLE'] . "</td>";
+            echo "<td>" . $row['PRODUCT_QUANTITY'] . "</td>";
+            echo "<td>" . $row['DATE_CREATE'] . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </tbody>
+</table>
